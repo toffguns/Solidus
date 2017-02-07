bf399455b748066e85ce6ac94d2f7607e245fdfc
Douglas Chun <dugchun@aol.com>
2017-02-07 14:45:23 -0800

    Initialized Solidus project and amended asset pipeline

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,24 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//
+//= require jquery
+//= require jquery_ujs
+//= twitter/bootstrap
+//= require turbolinks
+//= require jquery.themepunch.tools.min
+//= require jquery.themepunch.revolution.min
+//= require owl.carousel
+//= require jquery.selectbox-0.1.3.min
+//= require jquery.syotimer
+//= require custom
+//= require_tree .
