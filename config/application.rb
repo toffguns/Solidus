435573019afdb0fe66262fabe560ea85a3c1c620
Douglas Chun <dugchun@aol.com>
2017-02-10 13:43:12 -0800

    modified sidebar, navbar, footer with wrapbootstrap template

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Solidus
   class Application < Rails::Application
-    
+
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
@@ -34,6 +34,7 @@ module Solidus
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+  
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
